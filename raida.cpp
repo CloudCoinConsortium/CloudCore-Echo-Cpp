f45f8a2e2a6ec01a923bbe87d1a7b7b2b45fd6f0
Dipen Chauhan <get.dipen@gmail.com>
2019-01-14 19:41:41 -0800

    Added getters and setters

--- raida.cpp
+++ raida.cpp
@@ -1,4 +1,6 @@
 #include "raida.h"
+#include <iostream>
+using namespace std;
 
 void Raida::echo()
 {
@@ -7,10 +9,45 @@ void Raida::echo()
 
 void Raida::displayOutput()
 {
-
+	cout << m_https_response;
 }
 
-bool Raida::isAvailable()
+// Getters and Setters
+unsigned int Raida::getIndex()
 {
+	return m_index;
+}
+void Raida::setIndex(unsigned int index)
+{
+	m_index = index;
+}
 
+string Raida::getServerName()
+{
+	return m_server_name;
+}
+
+string Raida::getStatus()
+{
+	return m_status;
+}
+
+string Raida::getMessage()
+{
+	return m_message;
+}
+
+string Raida::getVersion()
+{
+	return m_version;
+}
+
+string Raida::getTime()
+{
+	return m_time;
+}
+
+bool Raida::getAvailable()
+{
+	return m_available;
 }
--- raida.h
+++ raida.h
@@ -1,25 +1,43 @@
 /**
- * Raid class
+ * Raida class
  * Author: Dipen Chauhan
  * Description: Class for RAIDA. 
  * 		Member variables get populated after echo() function is called.
  */
+#ifndef RAIDA_H
+#define RAIDA_H
+
+#include <string>
+using namespace std;
+
 class Raida
 {
+	unsigned int m_index; // RAIDA number. Eg. raida1.cloudcoin.global
 	string m_server_name;
 	string m_status;
 	string m_message;
 	string m_version;
 	string m_time;
-public:
-	
-	void echo();
-	void displayOutput();
-	bool isAvailable();
+	string m_https_response;
+	bool m_available; // True when "status" is "ready". False otherwise
 
+public:	
+	Raida()
+	{
+		m_available = false;
+	}
+	void echo();	// Contacts the RAIDA server
+	void displayOutput();	// Displays output from RAIDA server
+
+	// Getters and Setters
+	unsigned int getIndex();
+	void setIndex(unsigned int index);
 	string getServerName();
 	string getStatus();
 	string getMessage();
 	string getVersion();
 	string getTime();
+	bool getAvailable();
 };
+
+#endif /* RAIDA_H */
